012e36d3028b9fa73d6cc5e2d0d04abbe1852ef3
Jess <jlbachel@umich.edu>
2025-01-08 21:49:59 -0500

    Initial Commit

---
+++ LanguageTime/LanguageTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageTimeApp.swift
+//  LanguageTime
+//
+//  Created by Jess  on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LanguageTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LanguageTimeTests/LanguageTimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageTimeTests.swift
+//  LanguageTimeTests
+//
+//  Created by Jess  on 1/8/25.
+//
+
+import Testing
+@testable import LanguageTime
+
+struct LanguageTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
