6893328b7d141fbea1408f10dc0992ede9137e9b
Jess <jlbachel@umich.edu>
2025-01-14 15:57:59 -0500

    sure am doing some things (separating views into separate view files)

---
+++ LanguageTime/HeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderView.swift
+//  LanguageTime
+//
+//  Created by Jess  on 1/14/25.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeaderView()
+}
